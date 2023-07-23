75b7f41167c958390c953ee9565d22a9f5f63115
saeed dabaghian <saeed.dabaghian@gmail.com>
2023-07-23 09:50:46 +0330

    hw10

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+                Object resource1 = new Object();
+                Object resource2 = new Object();
+
+
+                Thread t1 = new Thread(() -> {
+                    synchronized (resource1) {
+                        System.out.println("Thread 1: Holding resource 1...");
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("Thread 1: Waiting for resource 2...");
+                        synchronized (resource2) {
+                            System.out.println("Thread 1: Holding resource 1 and resource 2...");
+                        }
+                    }
+                });
+
+
+                Thread t2 = new Thread(() -> {
+                    synchronized (resource2) {
+                        System.out.println("Thread 2: Holding resource 2...");
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("Thread 2: Waiting for resource 1...");
+                        synchronized (resource1) {
+                            System.out.println("Thread 2: Holding resource 2 and resource 1...");
+                        }
+                    }
+                });
+
+                t1.start();
+                t2.start();
+
+
+                try {
+                    t1.join();
+                    t2.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
